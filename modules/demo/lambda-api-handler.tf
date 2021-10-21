4b004948a6dfeb9f3a4a87d7e34db3985e9839ea
Salvatore Cirone <s.cirone@reply.it>
2021-10-21 09:41:12 +0200

    fix: cross-os valid lambda definition

--- modules/demo/lambda-api-handler.tf
+++ modules/demo/lambda-api-handler.tf
@@ -1,9 +1,9 @@
 resource "aws_lambda_function" "api_handler" {
-  filename         = "${path.module}/lambda/api-handler/build.zip"
+  filename         = "${path.module}/lambda/api-handler-build.zip"
   function_name    = "${var.userid}-${var.student}-${var.project}-api-handler"
   role             = var.lambda_role.arn
   source_code_hash = data.archive_file.api_handler.output_base64sha256
-  handler          = "index.handler"
+  handler          = "src/index.handler"
   runtime          = "nodejs14.x"
   timeout          = 3
 
@@ -20,17 +20,12 @@ resource "null_resource" "yarn_install_api_handler" {
     working_dir = "${path.module}/lambda/api-handler"
     command     = "yarn --frozen-lockfile --mutex network"
   }
-
-  provisioner "local-exec" {
-    working_dir = "${path.module}/lambda/api-handler"
-    command     = "yarn build"
-  }
 }
 
 data "archive_file" "api_handler" {
   type        = "zip"
-  source_dir  = "${path.module}/lambda/api-handler/dist/"
-  output_path = "${path.module}/lambda/api-handler/build.zip"
+  source_dir  = "${path.module}/lambda/api-handler/"
+  output_path = "${path.module}/lambda/api-handler-build.zip"
 
   depends_on = [
     null_resource.yarn_install_api_handler
